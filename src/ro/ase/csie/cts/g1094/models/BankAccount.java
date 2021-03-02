a9a992c721eb97a3dc7cef5247ac6518fc87bcca
Alexandra Pantiru <alexandrapantiru68@gmail.com>
2021-03-02 15:30:57 +0200

    Last commit s1

---
+++ src/ro/ase/csie/cts/g1094/models/BankAccount.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.g1094.models;
+
+public abstract class BankAccount extends Account{
+    protected double balance;
+    protected String iban;
+
+    public BankAccount(String iban, double balance) {
+        this.iban = iban;
+        this.balance = balance;
+
+    }
+
+    @Override
+    public Object getBalance() {
+        return this.balance;
+    }
+}
