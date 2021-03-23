472f6d5f1fd9e5030b7acde7ac4d2a6f7fb45a80
Alexandra Pantiru <alexandrapantiru68@gmail.com>
2021-03-23 14:21:25 +0200

    part 1 sem 4

---
+++ src/ro/ase/csie/cts/g1094/singleton/DbConnection.java
@@ -0,0 +1,48 @@
+package ro.ase.csie.cts.g1094.singleton;
+
+public class DbConnection {
+	
+	String connString;
+	String schema;
+	
+	private static DbConnection connection = null; //is static because it is an unique object (removed from obj area)
+	
+	private DbConnection() {
+		
+		System.out.println("Creating a connection object");
+		System.out.println("Loading config ...");
+		this.connString = "127.0.0.1";
+		this.schema = "cts";
+
+		
+	}
+
+	private DbConnection(String connString, String schema) {
+		super();
+		this.connString = connString;
+		this.schema = schema;
+	}
+	
+	
+	public static DbConnection getConnection() {
+		if(DbConnection.connection == null) {
+			connection = new DbConnection(); //the only time i create a connection
+			
+		}
+		return DbConnection.connection;
+	}
+	
+	
+	//not clean - misleading! - other will think they have multiple connections!
+	public static DbConnection getConnection(String connString, String schema) {
+		if(DbConnection.connection == null) {
+			connection = new DbConnection(connString, schema); //the only time i create a connection
+			
+		}
+		return DbConnection.connection;
+	}
+	
+	
+	
+
+}
